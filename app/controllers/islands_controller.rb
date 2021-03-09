fe36ffbf7271b753f76314481f85c355dad55a18
agustina ohan <agustina.oh@gmail.com>
2021-03-09 17:36:39 +0100

    new island form

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -8,8 +8,25 @@ class IslandsController < ApplicationController
     @island = Island.new
   end
 
+  def create
+    @island = Island.new(island_params)
+    @island.user = current_user
+
+    if @island.save
+      redirect_to @island
+    else
+      render :new
+    end
+  end
+
   def show
     @island = Island.find(params[:id])
   end
 
+  def island_params
+    params.require(:island).permit(
+      :name, :location, :description, :capacity, :facilities,
+      :access, :price_per_night, :photos
+    )
+  end
 end
