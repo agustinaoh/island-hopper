382919de773b7704cbb6931622731080d094fdc9
Guillaume Lafond <g.lafond@gmail.com>
2021-03-11 12:57:14 +0100

    added preset choices Arrays in Island model + new form

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,6 +1,7 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
+
   const locationInput = document.getElementById('island_location');
   if (locationInput) {
     places({ container: locationInput });
--- app/models/island.rb
+++ app/models/island.rb
@@ -4,4 +4,8 @@ class Island < ApplicationRecord
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  ACCESS = ["Boat", "Helicopter", "Plane", "Swim", "Parachute"]
+  FACILITIES = ["Pool", "Sauna", "Jacuzzi", "Tennis Court", "Helicopter filed", "Volcano"]
+
 end
