3c3dbd1320413c3b1c4eeb33c055e0d751d4527d
agustina ohan <agustina.oh@gmail.com>
2021-03-11 15:37:20 +0100

    bookings fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,12 @@
 class BookingsController < ApplicationController
-
   def new
     @island = Island.find(params[:island_id])
     @booking = Booking.new
   end
 
-  def show
-    @booking = Booking.find(params[:id])
-  end
+  # def show
+  #   @booking = Booking.find(params[:id])
+  # end
 
   def create
     @booking = Booking.new(booking_params)
@@ -22,30 +21,29 @@ class BookingsController < ApplicationController
     end
   end
 
-  def edit
-    @booking = Booking.find(params[:id])
-    @island = Island.find(params[:island_id])
-    @booking.island = @island
-    @booking.user = current_user
-  end
+  # def edit
+  #   @booking = Booking.find(params[:id])
+  #   @island = Island.find(params[:island_id])
+  #   @booking.island = @island
+  #   @booking.user = current_user
+  # end
 
-  def update
-    @booking = Booking.find(params[:id])
-    if @booking.update(booking_params)
-        redirect_to booking_path(@booking), notice: "The island owner has been notified that you updated your dates! :partying_face:"
-    else
-        render :edit
-    end
-  end
+  # def update
+  #   @booking = Booking.find(params[:id])
+  #   if @booking.update(booking_params)
+  #     redirect_to booking_path(@booking), notice: "The island owner has been notified that you updated your dates! :partying_face:"
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   def index
     @bookings = Booking.where(user: current_user)
   end
 
-    private
+  private
 
   def booking_params
-    params.require(:booking).permit(:arrive, :leave, :user_id, :island_id)
+    params.require(:booking).permit(:start_date, :end_date, :island_id)
   end
-
 end
